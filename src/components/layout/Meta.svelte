8d6d1c657b9554f69cc798a1876d2c15dc5aba10
Atharva Sehgal <atharva.sehgal@gmail.com>
2026-05-13 01:11:39 -0500

    resize og banner and fix og:image height

--- src/components/layout/Meta.svelte
+++ src/components/layout/Meta.svelte
@@ -31,7 +31,7 @@
 	/>
 	<meta property="og:image:type" content="image/png" />
 	<meta property="og:image:width" content="1200" />
-	<meta property="og:image:height" content="600" />
+	<meta property="og:image:height" content="630" />
 
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:site" content="https://formulacode.org/" />
